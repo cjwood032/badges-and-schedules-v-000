d90adfb1b56e7bf6c2fd7a2d644d81b59d09150d
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-20 08:22:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,15 @@
 
 # Write your code here.
 
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  attendees.map do |attendee|
-    "Hello, my name is #{attendee}."
+  array.each do |nombre|
+    "Hello, my name is #{nombre}."
   end
 end
 
