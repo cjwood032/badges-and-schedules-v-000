6f69d00fd5cafc009eefa4f5052a9696b6502d88
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-20 08:23:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,24 +7,24 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(array)
   array.each do |nombre|
     "Hello, my name is #{nombre}."
   end
 end
 
-def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
+def assign_rooms(array)
+  array.each_with_index.map do |nombre, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
 
-def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
+def printer(array)
+  batch_badge_creator(array).each do |badge|
     puts badge
   end
 
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
+  assign_rooms(array).each do |room|
+    puts room
   end
 end
