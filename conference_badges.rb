0aff9d946d87f05ea5e531773e3f5a645f1fda46
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-20 08:30:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.map do |nombre, index|
+  array.each_with_index.map do |nombre, index|
     "Hello, #{nombre}! You'll be assigned to room #{index+1}!"
   end
 end
